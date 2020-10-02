6a0b1473cee8ba3ed286c2ebf1331ed28622cfe5
Antoine Chassagne <antoine.chassagne@me.com>
2020-10-02 13:55:50 +0200

    :sparkles: Add double number function

--- handler.js
+++ handler.js
@@ -4,3 +4,18 @@ exports.hello = async (event) => {
     body: "Hello 👋",
   };
 };
+
+exports.double = async (event) => {
+  if (!event || (event && !event.queryStringParameters)) {
+    return {
+      statusCode: 200,
+      body: "Should provide a number.",
+    };
+  }
+  const doubledValue = event.queryStringParameters * 2;
+
+  return {
+    statusCode: 200,
+    body: doubledValue,
+  };
+};
